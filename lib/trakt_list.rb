da5ab158602f3344b86832ca172709bf2704c583
R <raphy@hobbitton.at>
2017-07-09 12:53:35 +0100

    More debug

--- lib/trakt_list.rb
+++ lib/trakt_list.rb
@@ -61,7 +61,6 @@ class TraktList
                                                :sort => {:order => 'ascending', :method => 'label'}})
       end
       k.each do |m|
-        Speaker.speak_up("m is #{m}")
         next if complete.to_i > 0 && ['shows', 'episodes'].include?(type) && m['watchedepisodes'].to_i < m['episode'].to_i
         next if complete.to_i < 0 && ['shows', 'episodes'].include?(type) && m['watchedepisodes'].to_i >= m['episode'].to_i
         next if type == 'movies' && m['playcount'].to_i == 0
@@ -90,22 +89,28 @@ class TraktList
                else
                  0
                end
-    type_history = filter_type.include?('watched') ? get_watched(type, complete) : []
     list.reverse_each do |item|
       next if add_only.to_i > 0 && search_list(type[0...-1], item, old_list)
       title = item[type[0...-1]]['title']
       next if exception && exception.include?(title)
       case filter_type
         when 'watched', 'entirely_watched', 'partially_watched'
-          type_history.each do |h|
+          get_watched(type, complete).each do |h|
+            Speaker.speak_up("item is #{item} compared to #{h}")
             if h[type[0...-1]] && h[type[0...-1]]['ids']
               h[type[0...-1]]['ids'].each do |k, id|
+                Speaker.speak_up("item[type[0...-1]]['ids'][k] = #{item[type[0...-1]]['ids'][k].gsub(/\D/,'')}") rescue nil
+                Speaker.speak_up("id = #{id.gsub(/\D/,'')}") rescue nil
+                Speaker.speak_up("compare = #{item[type[0...-1]]['ids'][k] && item[type[0...-1]]['ids'][k].gsub(/\D/,'').to_i == id.gsub(/\D/,'').to_i}") rescue nil
                 if item[type[0...-1]]['ids'][k] && item[type[0...-1]]['ids'][k].gsub(/\D/,'').to_i == id.gsub(/\D/,'').to_i
                   list.delete(item)
                   break
                 end
               end
             end
+            Speaker.speak_up("item[type[0...-1]]['title']+item[type[0...-1]]['year'].to_s = #{item[type[0...-1]]['title']+item[type[0...-1]]['year'].to_s}") rescue nil
+            Speaker.speak_up("h[type[0...-1]]['title']+h[type[0...-1]]['year'].to_s = #{h[type[0...-1]]['title']+h[type[0...-1]]['year'].to_s}") rescue nil
+            Speaker.speak_up("compare = #{item[type[0...-1]]['title']+item[type[0...-1]]['year'].to_s == h[type[0...-1]]['title']+h[type[0...-1]]['year'].to_s}") rescue nil
             if item[type[0...-1]]['title']+item[type[0...-1]]['year'].to_s == h[type[0...-1]]['title']+h[type[0...-1]]['year'].to_s
               list.delete(item)
               break
