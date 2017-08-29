c27c68a5fb2d0be4b3f19016a88b3c2cd51cf551
R <raphy@hobbitton.at>
2017-08-29 15:42:27 +0100

    Optimization movies title results

--- lib/media_info.rb
+++ lib/media_info.rb
@@ -3,7 +3,7 @@ class MediaInfo
   @last_tvmaze_req = Time.now - 1.day
 
   def self.clean_title(title)
-    title.gsub(/\(I+\) /,'')
+    title.gsub(/\(I+\) /,'').gsub(' (Video)','')
   end
 
   def self.tv_series_search(title, tvdb_id = '')
