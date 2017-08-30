7f606217b8a7866cfe5f0bb6ed5cd67af4c10cd3
R <raphy@hobbitton.at>
2017-08-30 18:41:48 +0100

    fix

--- lib/media_info.rb
+++ lib/media_info.rb
@@ -3,7 +3,7 @@ class MediaInfo
   @last_tvmaze_req = Time.now - 1.day
 
   def self.clean_title(title)
-    title.gsub(/\(I+\) /,'').gsub(' (Video)','').gsub(/\(TV .+\)/, '')
+    title.gsub(/\(I+\) /,'').gsub(' (Video)','').gsub(/\(TV .+\)/, '') rescue title
   end
 
   def self.tv_series_search(title, tvdb_id = '')
@@ -29,13 +29,13 @@ class MediaInfo
   def self.movie_title_lookup(title)
     movies = moviedb_search(title)
     if movies.empty?
-      return [[title, nil]], false
+      return [[title, '']], false
     else
       return movies.map { |m| [clean_title(m.title), m.url]}, true
     end
   rescue => e
     Speaker.tell_error(e, "MediaInfo.movie_title_lookup")
-    return [[title, nil]], false
+    return [[title, '']], false
   end
 
   def self.moviedb_search(title, no_output = false)
