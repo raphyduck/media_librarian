d72a115fe2c2d5b48a2ea902194a63e41553f607
Raphy <raphyduck@users.noreply.github.com>
2025-10-24 18:18:14 +0100

    Handle SMTP CRL verification via ssl_context override

--- init/email.rb
+++ init/email.rb
@@ -1,6 +1,7 @@
 # Configure email alerts
 require 'openssl'
 require_relative '../boot/librarian'
+require 'mail/network/delivery_methods/smtp'
 
 app = MediaLibrarian::Boot.application
 app.email_templates = File.expand_path('../app/mailer_templates', __dir__)
@@ -8,9 +9,25 @@ FileUtils.mkdir_p(app.email_templates) unless File.exist?(app.email_templates)
 app.email = app.config['email']
 
 if app.email
+  module Mail
+    class SMTP
+      module VerifyCallback
+        def ssl_context(*)
+          super.tap do |context|
+            skip_crl_error = OpenSSL::X509::V_ERR_UNABLE_TO_GET_CRL
+            context.verify_callback = lambda do |preverify_ok, store|
+              preverify_ok || store&.error == skip_crl_error
+            end
+          end
+        end
+      end
+
+      prepend VerifyCallback
+    end
+  end
+
   Hanami::Mailer.configure do
     root app.email_templates
-    skip_crl_error = OpenSSL::X509::V_ERR_UNABLE_TO_GET_CRL
     delivery_method :smtp,
                     address:              app.email['host'],
                     port:                 app.email['port'],
@@ -19,9 +36,6 @@ if app.email
                     password:             app.email['password'],
                     authentication:       app.email['auth_type'],
                     enable_starttls_auto: true,
-                    ssl_context_params:   {
-                      verify_mode:    OpenSSL::SSL::VERIFY_PEER,
-                      verify_callback: ->(ok, store) { ok || store&.error == skip_crl_error }
-                    }
+                    openssl_verify_mode:  OpenSSL::SSL::VERIFY_PEER
   end.load!
 end
