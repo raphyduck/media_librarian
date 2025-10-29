a23ae174bd40d07adbef0d9666ce05bc7d9a81e1
Raphy <raphyduck@users.noreply.github.com>
2025-10-29 20:07:27 +0100

    Add tracker login service and CLI command

---
+++ app/tracker_tools.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class TrackerTools
+  include MediaLibrarian::AppContainerSupport
+
+  class << self
+    def login(tracker_name, no_prompt: 0)
+      raise ArgumentError, 'tracker_name is required' if tracker_name.to_s.strip.empty?
+
+      MediaLibrarian::Services::TrackerLoginService
+        .new(app: app)
+        .login(tracker_name.to_s, no_prompt: no_prompt)
+    end
+  end
+end
