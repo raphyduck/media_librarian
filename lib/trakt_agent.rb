647fcd208b1beeffb1aaed5ec04c5e3646e9abb5
raphyduck <raphy@hobbitton.at>
2025-12-21 14:24:53 +0100

    Add back debug info

--- lib/trakt_agent.rb
+++ lib/trakt_agent.rb
@@ -10,7 +10,10 @@ class TraktAgent
   def self.method_missing(name, *args)
     segments = name.to_s.split('__')
     return unless segments[0] && segments[1]
-
+    MediaLibrarian.app.speaker.speak_up(
+      "Running TraktAgent.#{segments[0]}__#{segments[1]}(#{DataUtils.format_string(args).join(', ')})",
+      0
+    ) if Env.debug?
     target = MediaLibrarian.app.trakt.public_send(segments[0])
     response = target.public_send(segments[1], *args)
 
