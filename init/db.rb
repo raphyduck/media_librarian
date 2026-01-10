7bc065453ad1e75e83658ebfcd90ef1fb15c5ab8
Raphy <raphyduck@users.noreply.github.com>
2026-01-10 16:02:55 +0000

    Skip DB init in client mode

--- init/db.rb
+++ init/db.rb
@@ -1,8 +1,10 @@
 require_relative '../boot/librarian'
 
-# Set up and open app DB
-container = MediaLibrarian::Boot.container
-app = container.application
+unless ENV['MEDIA_LIBRARIAN_CLIENT_MODE'] == '1'
+  # Set up and open app DB
+  container = MediaLibrarian::Boot.container
+  app = container.application
 
-# Accessing through the container eagerly materializes the shared services.
-app.db
+  # Accessing through the container eagerly materializes the shared services.
+  app.db
+end
