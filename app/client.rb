fb974105ba9b20b88f444e38ec95f20b00cdd991
Raphy <raphyduck@users.noreply.github.com>
2026-01-12 12:15:01 +0100

    Restore HTTP fallback for CLI client

--- app/client.rb
+++ app/client.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'openssl'
 require 'socket'
+require 'httparty'
 
 class Client
   include MediaLibrarian::AppContainerSupport
@@ -49,22 +51,18 @@ class Client
   private
 
   def perform(method, path, options = {})
-    payload = {
-      'method' => method.to_s.upcase,
-      'path' => path,
-      'headers' => default_headers.merge(options.fetch(:headers, {})),
-      'body' => options[:body]
-    }
-    response_line = nil
+    socket_error = nil
 
-    UNIXSocket.open(socket_path) do |socket|
-      socket.write(JSON.dump(payload) + "\n")
-      response_line = socket.gets
+    if use_socket?
+      response_line = socket_request(method, path, options)
+      return parse_socket_response(response_line) if response_line
+
+      socket_error = @last_socket_error
     end
 
-    parse_response(response_line)
-  rescue Errno::ENOENT, Errno::ECONNREFUSED, Errno::ECONNRESET, IOError => e
-    { 'status_code' => 503, 'error' => connection_error_message(e) }
+    return perform_http(method, path, options) if http_configured?
+
+    { 'status_code' => 503, 'error' => connection_error_message(socket_error) }
   end
 
   attr_reader :control_token
@@ -107,6 +105,89 @@ class Client
     api_options['socket_path'] || SOCKET_PATH
   end
 
+  def use_socket?
+    path = socket_path
+    return false if path.to_s.empty?
+
+    File.socket?(path)
+  rescue Errno::ENOENT, Errno::EACCES
+    false
+  end
+
+  def socket_request(method, path, options)
+    payload = {
+      'method' => method.to_s.upcase,
+      'path' => path,
+      'headers' => default_headers.merge(options.fetch(:headers, {})),
+      'body' => options[:body]
+    }
+    response_line = nil
+
+    UNIXSocket.open(socket_path) do |socket|
+      socket.write(JSON.dump(payload) + "\n")
+      response_line = socket.gets
+    end
+
+    response_line
+  rescue Errno::ENOENT, Errno::ECONNREFUSED, Errno::ECONNRESET, IOError => e
+    @last_socket_error = e
+    nil
+  end
+
+  def perform_http(method, path, options)
+    response = HTTParty.send(method, path, httparty_options(options))
+    parse_http_response(response)
+  rescue Errno::ECONNREFUSED, Net::OpenTimeout, Net::ReadTimeout, OpenSSL::SSL::SSLError => e
+    { 'status_code' => 503, 'error' => connection_error_message(e) }
+  end
+
+  def ssl_enabled?
+    truthy?(api_options['ssl_enabled'])
+  end
+
+  def resolve_ssl_verify_mode(mode)
+    return mode if mode.is_a?(Integer)
+
+    case mode.to_s.downcase
+    when '', 'none', 'off', 'false'
+      OpenSSL::SSL::VERIFY_NONE
+    when 'peer'
+      OpenSSL::SSL::VERIFY_PEER
+    when 'fail_if_no_peer_cert', 'force_peer', 'require'
+      OpenSSL::SSL::VERIFY_PEER | OpenSSL::SSL::VERIFY_FAIL_IF_NO_PEER_CERT
+    else
+      OpenSSL::SSL::VERIFY_NONE
+    end
+  end
+
+  def truthy?(value)
+    case value
+    when true then true
+    when false, nil then false
+    when String then value.match?(/\A(true|1|yes|on)\z/i)
+    when Numeric then !value.zero?
+    else
+      !!value
+    end
+  end
+
+  def http_configured?
+    !api_options['bind_address'].to_s.empty? && !api_options['listen_port'].to_s.empty?
+  end
+
+  def base_uri
+    "#{ssl_enabled? ? 'https' : 'http'}://#{api_options['bind_address']}:#{api_options['listen_port']}"
+  end
+
+  def httparty_options(extra = {})
+    headers = default_headers.merge(extra.fetch(:headers, {}))
+    ssl_options = httparty_ssl_options
+
+    { base_uri: base_uri, headers: headers, timeout: 120 }
+      .merge(ssl_options)
+      .merge(extra.reject { |key, _| key == :headers })
+  end
+
   def default_headers
     return {} unless control_token
     { 'X-Control-Token' => control_token }
@@ -116,7 +197,29 @@ class Client
     default_headers.merge('Content-Type' => 'application/json')
   end
 
-  def parse_response(response_line)
+  def httparty_ssl_options
+    return {} unless ssl_enabled?
+
+    options = {}
+    verify_mode = resolve_ssl_verify_mode(api_options['ssl_verify_mode'])
+    options[:verify] = verify_mode != OpenSSL::SSL::VERIFY_NONE unless verify_mode.nil?
+    options[:ssl_verify_mode] = verify_mode unless verify_mode.nil?
+
+    ca_path = api_options['ssl_ca_path']
+    if ca_path && !ca_path.to_s.empty?
+      options[:ssl_ca_path] = ca_path if File.directory?(ca_path)
+      options[:ssl_ca_file] = ca_path if File.file?(ca_path)
+    end
+
+    options
+  end
+
+  def parse_http_response(response)
+    body = response.body.to_s.empty? ? nil : JSON.parse(response.body)
+    { 'status_code' => response.code.to_i, 'body' => body }
+  end
+
+  def parse_socket_response(response_line)
     raise JSON::ParserError if response_line.to_s.strip.empty?
 
     parsed = JSON.parse(response_line)
@@ -131,6 +234,8 @@ class Client
     case error
     when Errno::ECONNREFUSED
       'Failed to connect to daemon'
+    when Net::OpenTimeout, Net::ReadTimeout
+      'Timed out waiting for daemon response'
     when Errno::ENOENT
       'Daemon socket missing'
     else
